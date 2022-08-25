0f390b883e700821059064a1edfb1c0560ecfddc
devspacerj <rf2014souza@gmail.com>
2022-08-24 23:58:49 -0300

    22/08/2022 criado o sistema de validação de login

--- src/rotas/routes.ts
+++ src/rotas/routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
-import { Request, Response } from 'express';
+import { Request, Response , NextFunction } from 'express';
 
-import validarDados, { validarToken } from '../util/validacao';
+import  { validarDados, validarToken } from '../util/validacao';
 
  const router = Router();
 
-router.post('/login', validarDados , async ( req : Request , res : Response) => {
+router.post('/login', validarDados , async (req: Request, res: Response, next: NextFunction) => {
+  console.log('olá')
 })
 
-
-router.get('/',validarToken , async (req: Request, res: Response) => {
+router.get('/',validarToken , async (req: Request, res: Response, next: NextFunction) => {
+  console.log('olá')
 })
+router.get('/produtos', (req: Request, res: Response ) => {
+ })
+
+
+
 
 export default router;
