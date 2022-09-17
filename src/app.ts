6a67377953f43910bc58f67453f56dc0917dbc2a
devspacerj <rf2014souza@gmail.com>
2022-09-17 11:46:10 -0300

    17/09/2022 validação formulário pelo server

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,16 @@ import logger from 'morgan';
  const app = express();
 
 app.use(express.json());
-app.use(cors({credentials : true }));
+app.use(cors());
 app.use(logger('dev'));
-
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 app.use('/', router);
 
 
