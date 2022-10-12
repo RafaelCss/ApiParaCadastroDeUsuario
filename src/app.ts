849fcffc32c47fe7c29fc23018517fadee79fcb7
devspacerj <rf2014souza@gmail.com>
2022-10-12 10:25:09 -0300

    teste heroku

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import cors from 'cors';
 import router  from './rotas/routes';
 import logger from 'morgan';
 
- const app = express();
-
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
+
     next();
 });
 app.use('/', router);
