5466b26c97439c76e502dc429cfbb086fdb717e0
devspacerj <rf2014souza@gmail.com>
2022-10-12 10:30:51 -0300

    teste heroku

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import router  from './rotas/routes';
 import logger from 'morgan';
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(logger('dev'));
 app.use((req, res, next) => {
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-
+    app.use(cors());
     next();
 });
 app.use('/', router);
