22e885dfbf5dece691354ee4c4057fbbd9aacf44
devspacerj <rf2014souza@gmail.com>
2022-10-12 11:42:59 -0300

    teste heroku

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     console.log('chegou!!')
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://cadastroprodutosteste.web.app/");
 	//Quais são os métodos que a conexão pode realizar na API
      res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
