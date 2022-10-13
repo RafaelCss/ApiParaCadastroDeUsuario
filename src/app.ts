bccae4db9dadc89bd2dab146e2e772e7965d5f70
devspacerj <rf2014souza@gmail.com>
2022-10-13 12:39:22 -0300

    12/10/2022 url produção

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,10 @@ import router  from './rotas/routes';
 import logger from 'morgan';
 ;
 const app = express();
-app.use(cors());
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     console.log('chegou!!')
-    res.header("Access-Control-Allow-Origin", "https://cadastroprodutosteste.web.app/");
+    res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
      res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
